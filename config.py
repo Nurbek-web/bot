b2810c52461ae96df500cc23d5b1831d76ff9a82
Nurbek-web <78167331+Nurbek-web@users.noreply.github.com>
2021-03-30 00:06:43 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# Can be multiple prefixes, like this: ("!", "?")
+BOT_PREFIX = ("+")
+TOKEN = "Nzg3NjYxOTEyMzIxMTYzMjg0.X9YNUQ.BxkZIAED1aCkbLWQxC63q0KqHtI"
+APPLICATION_ID = "YOUR_APPLICATION_ID"
+OWNERS = [123456789, 987654321]
+BLACKLIST = []
+
+# Bot colors
+main_color = 0xD75BF4
+error = 0xE02B2B
+success = 0x42F56C
+warning = 0xF59E42
+info = 0x4299F5
